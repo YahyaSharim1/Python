c351c5e85adbd005e45794e1907dd4414dcf5111
Yahya Sharim <ash.yahya04@gmail.com>
2026-02-21 16:50:33 +0530

    Add files via upload

---
+++ act.py
@@ -0,0 +1,23 @@
+n= input ("enter your name  ")
+if n.isalpha():
+    print("your name is",n)
+else:
+    print ("invalid")
+
+a=input ("enter your age  ")
+if a.isdigit():
+    print ("you are",a)
+else:
+    print ("invalid")
+    
+p=input ("enter your password  ")
+if a.isalnum():
+    print ("correct",a)
+else:
+    print ("invalid")
+    
+pn=input ("enter your age  ")
+if a.isdigit():
+    print ("you are",a)
+else:
+    print ("invalid")
