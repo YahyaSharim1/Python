0f6dedf29d3c99d1ff50622d5863c33378abe850
Yahya Sharim <ash.yahya04@gmail.com>
2026-02-28 16:49:07 +0530

    Add files via upload

---
+++ Range 2.py
@@ -0,0 +1,21 @@
+#num=[1,2,3,4,5]
+#for n in num:
+    #print (n)
+    
+#for index in range(len(num)):
+    #print (num[index])
+
+num2=[5,4,2,1]
+
+#print (num2)
+#num2.append(3)
+#print(num2)
+
+#num2.insert(2,3)
+#print (num2)
+
+num2.pop()
+print (num2)
+
+num2.pop(0)
+print (num2)
